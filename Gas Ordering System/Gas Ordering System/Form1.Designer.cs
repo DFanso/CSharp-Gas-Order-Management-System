0183e130df59abe2d5697c52be1a7319aea22447
Leo Gavin <leogavin123@outlook.com>
2022-04-05 18:51:58 +0530

    Image

--- Gas Ordering System/Gas Ordering System/Form1.Designer.cs
+++ Gas Ordering System/Gas Ordering System/Form1.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::Gas_Ordering_System.Properties.Resources.Screenshot__1_;
-            this.pictureBox1.Location = new System.Drawing.Point(217, 174);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(98, 70);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(387, 356);
+            this.pictureBox1.Size = new System.Drawing.Size(1168, 644);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
